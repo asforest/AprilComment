2f857ba0773a9c1fb005755ba12b877f27284222
innc11 <aprilforests@qq.com>
2021-05-07 08:13:26 +0800

    refactor: AprilComment.create()重命名为mount()

--- src/index.ts
+++ src/index.ts
@@ -87,27 +87,37 @@ export default class AprilComment
 
     /**
      * 创建评论系统的DOM
+     * 
+     * @deprecated since 0.2.7. use mount() instead
      */
     async create()
     {
-        this.mainWidget = new AprilCommentWidget({
-            propsData: { owner: this }
-        }).$mount('#'+this.opt.el)
-
-        this.editorWidget = this.mainWidget.$refs.editor as Vue
-        this.profileWidget = this.mainWidget.$refs.profile as Vue
-        
-        await this.refresh()
-
-        if(typeof this.opt.smilies == 'object')
-            this.smilieManager?.loadFromObject(this.opt.smilies as object)
-        this.editorWidget.smiliesComponet.$forceUpdate()
-	    this.editorWidget.smiliesComponet.defaultSmilieSet()
-	    this.mainWidget.update()
-
-        if(this.opt.focusOnComment)
-            this.focusOnComment()
+        this.mount()
     }
+
+    /**
+     * 创建评论系统的DOM
+     */
+     async mount()
+     {
+         this.mainWidget = new AprilCommentWidget({
+             propsData: { owner: this }
+         }).$mount('#'+this.opt.el)
+ 
+         this.editorWidget = this.mainWidget.$refs.editor as Vue
+         this.profileWidget = this.mainWidget.$refs.profile as Vue
+         
+         await this.refresh()
+ 
+         if(typeof this.opt.smilies == 'object')
+             this.smilieManager?.loadFromObject(this.opt.smilies as object)
+         this.editorWidget.smiliesComponet.$forceUpdate()
+         this.editorWidget.smiliesComponet.defaultSmilieSet()
+         this.mainWidget.update()
+ 
+         if(this.opt.focusOnComment)
+             this.focusOnComment()
+     }
 
     /**
      * 聚焦到目标评论上（仅当从通知邮件内的链接跳转过来时）
