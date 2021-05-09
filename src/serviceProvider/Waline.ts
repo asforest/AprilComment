372ce974e897e07271e6b1d321b9502dcca97d47
innc11 <aprilforests@qq.com>
2021-05-09 19:36:08 +0800

    refactor: moved the convert function from utils to Waline.ts

--- src/serviceProvider/Waline.ts
+++ src/serviceProvider/Waline.ts
@@ -3,9 +3,12 @@ import UnknownException from "../exception/UnknownException";
 import CommentingModel from "../interface/CommentingModel";
 import CommentModel from "../interface/CommentModel";
 import RecentComment from "../interface/RecentComment";
-import { getAvatarByMail, objectToQueryParamter, sanitizeThroughly, useDefault } from "../utils/Utils";
-import WalineHtml2Markdown from "../utils/WalineHtml2Markdown";
+import { getAvatarByMail, sanitize, sanitizeThroughly, useDefault } from "../utils/Utils";
 import ServiceProvider from "./ServiceProvider";
+import cheerio from "cheerio";
+const katex = require('katex')
+const html2md = require('html-to-md')
+require('katex/dist/katex.css')
 
 export default class Waline extends ServiceProvider
 {
@@ -239,5 +242,65 @@ export default class Waline extends ServiceProvider
 
             return temp
 
+    }
+
+    html2md(walineHtml: string)
+    {
+        // 去除前后p标签
+        walineHtml = walineHtml.trim().replace(new RegExp("^<p>"), '').replace(new RegExp("</p>$"), '')
+    
+        // 提取出原始评论数据（去除html标签）
+        // third argument to disable the present of <html>, <head> and <body>
+        let $ = cheerio.load(walineHtml, null, false)
+    
+        // 移除p元素
+        // if($('p').length == 1)
+        //     $ = cheerio.load($('p').html() as string, null, false)
+    
+        // 移除At字样
+        if($('a.at[href]').length > 0)
+        {
+            $('a.at[href]').remove()
+            $ = cheerio.load($.html().replace(new RegExp("^ +, +"), ''), null, false)
+        }
+    
+        // 还原数学公式
+        $('span.katex').each((i, e) => {
+            let rawFormula = $(e).find('math semantics annotation').text()
+            $(e).replaceWith('\n$$'+rawFormula+'$$\n')
+        })
+    
+        // 还原换行/净化标签
+        let htmlText = $.html().replace(new RegExp("(<br>|<br/>|<br />)", 'g'), '\n')
+        let htmlmd = html2md(sanitize(htmlText))
+    
+        // 重新渲染数学公式
+        let reg = /\$?\$([^$]+)\$\$?/
+        let matches = reg.exec(htmlmd)
+        let maxTimes = 1000
+        while(matches != null)
+        {
+            let matchedText = matches[0]
+            let position = matches['index']
+    
+            let countof$ = matchedText.startsWith('$$')? 2:1
+            let formulaText = matchedText.substring(countof$, matchedText.length-countof$)
+            let formulaHtml = katex.renderToString(formulaText, {
+                throwOnError: false,
+                displayMode: true,
+                // output: 'mathml',
+            })
+    
+            let before = htmlmd.substr(0, position)
+            let after = htmlmd.substring(position + matchedText.length)
+            htmlmd = before + formulaHtml + after
+    
+            if(maxTimes--<=0)
+                throw 'max times reached'
+            
+            matches = reg.exec(htmlmd)
+        }
+    
+        return htmlmd
     }
 }
