2f8183fe63a91b435cf2f973e1ea1c3ab2f5fcb0
innc11 <aprilforests@qq.com>
2021-05-13 23:28:43 +0800

    style: 增加了评论按钮的图标

--- src/widget/CommentEditor.vue
+++ src/widget/CommentEditor.vue
@@ -46,6 +46,7 @@
                 <div class="ac-button" v-on:click.left="onComment" v-on:click.right.prevent="onLogin">
                     <!-- 加载动画 -->
                     <div class="ac-submiting-indicator" v-show="showSubmitingAnimation"></div>
+                    <svg t="1620919659883" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1971" width="22" height="22"><path d="M224 768A138.666667 138.666667 0 0 1 85.333333 629.333333v-362.666666A138.666667 138.666667 0 0 1 224 128h576A138.666667 138.666667 0 0 1 938.666667 266.666667v362.666666A138.666667 138.666667 0 0 1 800 768h-244.821333L341.333333 928a53.333333 53.333333 0 0 1-85.290666-42.666667V768h-32z m309.930667-64h266.069333a74.666667 74.666667 0 0 0 74.666667-74.666667v-362.666666a74.666667 74.666667 0 0 0-74.666667-74.666667H224A74.666667 74.666667 0 0 0 149.333333 266.666667v362.666666c0 41.216 33.450667 74.666667 74.666667 74.666667h95.957333v160l213.973334-160z" p-id="1972"></path></svg>
                     评论
                 </div>
             </div>
@@ -423,13 +424,13 @@ export default Vue.extend({
         margin-top: 0.5rem;
         background: transparent;
         resize: vertical;
-        transition: all 0.7s ease;
+        transition: border-color 0.7s ease;
         border: 1px solid #50505017;
         word-break: break-all;
         border-radius: 4px;
         border-bottom-right-radius: 8px;
 
-        &:focus {
+        &:focus, &:hover, &:focus {
             border: 1px solid #86bb9d;
         }
     }
